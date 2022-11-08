34479a041dc8a6f83577f073a0e23003ec7c0234
unknown <leonardoazeredosjc@gmail.com>
2022-11-08 16:44:26 -0300

    nova branch e adicionado input para numero da loja

--- mediastore1.py
+++ mediastore1.py
@@ -1,7 +1,5 @@
 from pydoc import describe
 import pandas as pd
-import requests
-import json
 from datetime import date
 
 
@@ -9,19 +7,18 @@ from datetime import date
 Store = 0
 Weekly_Sales = 0
 linha = 0
-
+loja = int(input('Número da loja: '))
 
 df = pd.read_csv("Walmart.csv")
 
 for index, row in df.iterrows():
     Store = row['Store']
     Weekly_Salescsv = row['Weekly_Sales']
-    if Store == 1:
+    if Store == loja:
         Weekly_Sales = Weekly_Sales + Weekly_Salescsv
         print(Weekly_Sales)
-        linha = linha + 1
-    else:
-        print("Não é store 1")
+        linha = linha + 1   
 
+arquivo = open("media.txt" , "w+")
 
-print (Weekly_Sales/linha)
+print ("Media da loja" , Weekly_Sales/linha , file=arquivo)
